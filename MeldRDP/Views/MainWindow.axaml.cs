68f7f8981ad98ad5f79172b5d16994ceba2a57eb
meld-cp <18450687+meld-cp@users.noreply.github.com>
2025-03-27 00:08:33 +1300

    add key bindings

--- MeldRDP/Views/MainWindow.axaml.cs
+++ MeldRDP/Views/MainWindow.axaml.cs
@@ -1,15 +1,37 @@
 ﻿namespace MeldRDP.Views {
 	using Avalonia.Controls;
+	using Avalonia.Input;
+
+	using ReactiveUI;
 
 	public partial class MainWindow : Window {
 
 		public MainWindow() {
 			InitializeComponent();
+
+			this.KeyBindings.Add(new KeyBinding() {
+				Command = ReactiveCommand.Create(FocusOnSearch),
+				Gesture = new KeyGesture(Key.F3)
+			});
+
+			this.KeyBindings.Add(new KeyBinding() {
+				Command = ReactiveCommand.Create(RefreshConnections),
+				Gesture = new KeyGesture(Key.F5)
+			});
+
+		}
+		private void Window_Activated(object? sender, System.EventArgs e) {
+			this.FocusOnSearch();
 		}
 
-		private void Window_Activated(object? sender, System.EventArgs e) {
+		private void RefreshConnections() {
+			this.MainView.ViewModel?.RefreshConnections();
+		}
+
+		private void FocusOnSearch() {
 			this.MainView.SearchText.Focus();
 			this.MainView.SearchText.SelectAll();
 		}
+
 	}
 }
